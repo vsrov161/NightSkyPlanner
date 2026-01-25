50c0a6e539d771fee2414d5b2ada728eebf844c6
Vic Sergeev <133888588+vsrov161@users.noreply.github.com>
2026-01-25 15:32:21 +0300

    Update RateMeViewController.swift
    upd rate me screen, added safari link to github

--- NightSkyPlanner/ViewControllers/Settings/RateMeViewController.swift
+++ NightSkyPlanner/ViewControllers/Settings/RateMeViewController.swift
@@ -10,40 +10,25 @@ import SnapKit
 
 final class RateMeViewController: UIViewController {
     
-    private lazy var mainStackView: UIStackView = {
-        var stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.spacing = 1
-        stackView.alignment = .center
-        stackView.distribution = .fillProportionally
-        stackView.backgroundColor = .systemBackground
-        return stackView
-    }()
-    
-    private lazy var topStackView: UIStackView = {
-        var stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.spacing = 8
-        stackView.alignment = .center
-        stackView.distribution = .fill
-        return stackView
-    }()
-    
-    private lazy var bottomStackView: UIStackView = {
-        var stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.spacing = 8
-        stackView.alignment = .center
-        stackView.distribution = .fill
-        return stackView
-    }()
-    
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
+        label.textAlignment = .center
         label.font = .systemFont(ofSize: 26, weight: .semibold)
         label.textColor = .lightGray
         label.text = "Night Sky Planner"
         return label
+    }()
+    
+    private lazy var rateButton: UIButton = {
+        
+        var config = UIButton.Configuration.bordered()
+        config.title = "⭐ Rate on GitHub"
+        
+        let button  = UIButton(configuration: config, primaryAction: nil)
+        
+        button.addTarget(self, action: #selector(openGitHub), for: .touchUpInside)
+        
+        return button
     }()
     
     private lazy var closeButton: UIButton = {
@@ -68,15 +53,46 @@ final class RateMeViewController: UIViewController {
     }
     
     private func setupUI() {
-        view.addSubview(mainStackView)
-        mainStackView.addArrangedSubview(topStackView)
-        mainStackView.addArrangedSubview(bottomStackView)
+        let contentStack = UIStackView(arrangedSubviews: [
+            titleLabel,
+            rateButton,
+            closeButton
+        ])
         
-        topStackView.addArrangedSubview(titleLabel)
-        bottomStackView.addArrangedSubview(closeButton)
+        contentStack.axis = .vertical
+        contentStack.spacing = 24
+        contentStack.alignment = .center
+        contentStack.distribution = .fill
         
-        mainStackView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+        view.addSubview(contentStack)
+        
+        contentStack.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(14)
         }
+        
+        rateButton.snp.makeConstraints { make in
+            make.height.equalTo(44)
+            make.width.equalToSuperview()
+        }
+        
+        closeButton.snp.makeConstraints { make in
+            make.height.equalTo(44)
+            make.width.equalToSuperview()
+        }
+        
+    }
+    
+    // MARK: Actions
+    @objc private func openGitHub() {
+        guard let url = URL(string: "https://github.com/vsrov161/NightSkyPlanner"),
+              UIApplication.shared.canOpenURL(url) else {
+            return
+        }
+        
+        UIApplication.shared.open(url)
+        print("github button worked")
+        
+        self.dismiss(animated: false)
     }
 }
